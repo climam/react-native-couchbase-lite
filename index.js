eb3d358b7c388f33f51da3e33fdb98128d1b91b2
Cyril Limam <cl@booqee.com>
2017-12-05 23:56:26 +0100

    Removed the listener on app change state as this should be on the main app

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import {AppState, NativeModules} from "react-native";
-import Swagger from "swagger-client";
-import spec from "./spec.json";
+import { AppState, NativeModules } from 'react-native';
+import Swagger from 'swagger-client';
+import spec from './spec.json';
 import base64 from 'base-64';
 const Couchbase = NativeModules.ReactCBLite;
 
@@ -11,28 +11,25 @@ let manager, callback;
 Couchbase.init(url => {
   spec.host = url.split('/')[2];
 
-  new Swagger({spec: spec, usePromise: true})
-    .then(client => {
-      var encodedCredentials = "Basic " + base64.encode(url.split("//")[1].split('@')[0]);
-      client.clientAuthorizations.add("auth", new Swagger.ApiKeyAuthorization('Authorization', encodedCredentials, 'header'));
-      manager = client;
-      if (typeof callback == 'function') {
-        callback(manager);
-      }
-    });
-
-  // stop and start are needed because the OS appears to kill the listener when the app
-  // becomes inactive (when the screen is locked, or its put in the background)
-  AppState.addEventListener('change', (appState) => {
-    if (String(appState).match(/background/)) {
-      Couchbase.stopListener();
-    } else if (String(appState).match(/active/)) {
-      Couchbase.startListener();
+  new Swagger({ spec: spec, usePromise: true }).then(client => {
+    var encodedCredentials =
+      'Basic ' + base64.encode(url.split('//')[1].split('@')[0]);
+    client.clientAuthorizations.add(
+      'auth',
+      new Swagger.ApiKeyAuthorization(
+        'Authorization',
+        encodedCredentials,
+        'header'
+      )
+    );
+    manager = client;
+    if (typeof callback == 'function') {
+      callback(manager);
     }
   });
 });
 
-Couchbase.initRESTClient = function (cb) {
+Couchbase.initRESTClient = function(cb) {
   if (typeof manager !== 'undefined') {
     cb(manager); // If manager is already defined, don't wait.
   } else {
